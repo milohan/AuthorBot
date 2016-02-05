95d6e4862d46bda025fa9a70a0c21d5c9596821b
milohan <milohan96@gmail.com>
2016-02-04 16:49:38 -0800

    Update StringPair.java

--- src/java/StringPair.java
+++ src/java/StringPair.java
@@ -1,7 +1,21 @@
-
-public class StringPair extends Pair<String, String> {
-    public StringPair(String fst, String snd) {
-        super(fst,snd);
-    }
-    
-}
+-/**
+ - * Convenience class representing a pair of strings.
+ - * Defined so can write StringPair instead of Pair<String,String>
+ - * This works because no methods of type Pair return another element
+ - * of type Pair.
+ - * @author kim
+ - * @version 1/2011
+ - *  revised: added package --kpc 2/13
+ - *
+ - */
+  
+  public class StringPair extends Pair<String, String> {
+ -    
+ -    /**
+ -     * Create new pair from first, second
+ -     * @param fst  first element of pair
+ -     * @param snd  second element of pair
+ -     */
+      public StringPair(String fst, String snd) {
+          super(fst,snd);
+      }
